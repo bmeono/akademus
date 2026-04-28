b7ce5ce22c4171cfd1255198fd2f521a77aaa05b
Brian <bmeono@cip.org.pe>
2026-04-27 22:30:51 -0500

    Fix TS build errors for frontend

---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,12 @@
+declare module '*.css';
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.webp';
+declare module '*.ico';
+declare module '*.woff';
+declare module '*.woff2';
+declare module '*.ttf';
+declare module '*.eot';
